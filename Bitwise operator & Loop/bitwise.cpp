af3a4c8ed0f3fc8b08a255c48d75ef3d95a090ac
DarshanModi07 <darshanmodi016@gmail.com>
2025-06-01 15:36:56 +0530

    Add files via upload

---
+++ Bitwise operator & Loop/bitwise.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+// Output Predict Questions : 
+
+int main(){
+    int a=4,b=6;
+
+    cout<<(a&b)<<endl;
+    cout<<(a|b)<<endl;
+    cout<<(~a)<<endl;
+    cout<<(a^b)<<endl;
+}
