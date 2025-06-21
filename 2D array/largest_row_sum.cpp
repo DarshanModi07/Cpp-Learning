244ccba4c11dcb202544b1d718d30316f80802f0
DarshanModi07 <darshanmodi016@gmail.com>
2025-06-21 08:53:32 +0530

    Add files via upload

---
+++ 2D array/largest_row_sum.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int sum(int arr[100][100],int row,int col){
+    int sum[3] = {0},max = arr[0][0];
+     for(int i = 0 ; i < row ; i++ ){
+        for(int j  = 0 ; j < col ; j++ ){
+            sum[i] += arr[i][j];
+        }
+    }
+
+    for(int i = 0 ; i < row ; i++ ){
+        cout<<"Sum of "<<i<<"th row : "<<sum[i];
+        cout<<endl;
+    }
+
+    int index = 0;
+    for(int i = 0 ; i < row ; i++ ){
+        if(sum[i] > max){
+            max = sum[i];
+            index = i;
+        }
+    }
+    cout << "Largest row sum is: " << max << " in the " << index <<" row." <<endl;
+}
+
+int main(){
+    int arr[100][100];
+    int rows, cols;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    cout<<endl;
+    cout << "Enter the number of columns: ";
+    cin >> cols;
+    cout<<endl;
+
+    // Input elements into the 2D array
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            cout << "Enter element at position (" << i << "," << j << "): ";
+            cin >> arr[i][j];
+        }
+    }
+
+    sum(arr,rows,cols);
+}
